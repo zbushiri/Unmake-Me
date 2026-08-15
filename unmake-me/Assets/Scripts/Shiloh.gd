da09f549b8e7c28426adaa24c8c465c73a33a476
Zaki <bushirizaki@gmail.com>
2026-08-15 11:44:59 -0400

    upd

--- unmake-me/Assets/Scripts/Shiloh.gd
+++ unmake-me/Assets/Scripts/Shiloh.gd
@@ -5,11 +5,18 @@ class_name PlayerController
 @export var jump_power = 10.0
 @export var animation_player : AnimationPlayer
 @export var sprite : Sprite2D
-
+@onready var actionable_finder: Area2D = $aFinder
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
 
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		var Actionable = actionable_finder.get_overlapping_areas()
+		if Actionable.size() > 0:
+			Actionable[0].action()
+			return
+			
 func _input(event):
 	# Handle jump.
 	if event.is_action_pressed("jump") and is_on_floor():
